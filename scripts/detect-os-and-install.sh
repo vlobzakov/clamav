f6083ffd053aef69a3a735ae1c2da81b6f57d8ed
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-04-29 23:31:42 +0200

    Update detect-os-and-install.sh

--- scripts/detect-os-and-install.sh
+++ scripts/detect-os-and-install.sh
@@ -4,14 +4,17 @@ detect=$(cat /etc/*-release | grep 'ID=')
 if [[ $detect == *"debian"* ]]; then
 	sudo apt-get update
 	sudo apt-get install clamav
+	sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
 elif [[ $detect == *"ubuntu"* ]]; then
 	sudo apt-get update
 	sudo apt-get install clamav
+	sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
 elif [[ $detect == *"centos"* ]]; then
     sudo yum install -y epel-release 
     sudo yum install -y clamav 
+    sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
 elif [[ $detect == *"alpine"* ]]; then
 	sudo apk add clamav
+	sudo sed -i -e "s/^Example/#Example/" /etc/clamav/freshclam.conf
 fi
 
-sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
