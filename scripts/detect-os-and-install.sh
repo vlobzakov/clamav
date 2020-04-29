be22c6e532ecb265109305c3f51cc9ce177ed23e
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-04-29 23:09:52 +0200

    Update detect-os-and-install.sh

--- scripts/detect-os-and-install.sh
+++ scripts/detect-os-and-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 detect=$(cat /etc/*-release | grep 'ID=')
-if [ $detect == *"debian"* ]; then
+if [[ $detect == *"debian"* ]]; then
 echo "debian"
 elif [[ $detect == *"centos"* ]]; then
 echo "centos"
