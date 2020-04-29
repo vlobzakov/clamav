2590392c58e97edff3c674cd1c5f91bc6f847d0b
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-04-29 23:18:30 +0200

    added uninstall script

---
+++ scripts/detect-os-and-uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+detect=$(cat /etc/*-release | grep 'ID=')
+if [ $detect == *"debian"* ]; then
+	sudo apt-get remove clamav
+elif [[ $detect == *"ubuntu"* ]]; then
+	sudo apt-get remove clamav
+elif [[ $detect == *"centos"* ]]; then
+    sudo yum remove -y epel-release 
+    sudo yum remove -y clamav 
+elif [[ $detect == *"alpine"* ]]; then
+	sudo apk del clamav
+fi
