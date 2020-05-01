ffc034b00c7dd8c5df55a93c2039702d1f6fc7d1
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-05-01 15:17:39 +0200

    added log file rights

--- scripts/detect-os-and-install.sh
+++ scripts/detect-os-and-install.sh
@@ -5,20 +5,19 @@ if [[ $detect == *"debian"* ]]; then
 	sudo apt-get update
 	sudo apt-get install clamav
 	sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
-	sudo freshclam
 elif [[ $detect == *"ubuntu"* ]]; then
 	sudo apt-get update
 	sudo apt-get install clamav
 	sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
-	sudo freshclam
 elif [[ $detect == *"centos"* ]]; then
     sudo yum install -y epel-release 
     sudo yum install -y clamav 
     sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
-    sudo freshclam
 elif [[ $detect == *"alpine"* ]]; then
 	sudo apk add clamav
 	sudo sed -i -e "s/^Example/#Example/" /etc/clamav/freshclam.conf
-	sudo freshclam
 fi
 
+sudo freshclam
+sudo touch /var/log/clamav.log
+sudo chgrp clamupdate /var/log/clamav.log
