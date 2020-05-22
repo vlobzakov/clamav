b1d731047a20e283e11e528d0dc31f1616e05c24
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-05-22 18:05:49 +0200

    added timestamp

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+date=$(date)
+
 freshclam
+echo "This Scan has been started at" $date >> /var/log/clamav/clamav.log
 clamscan --infected --recursive --remove --exclude-dir="^/sys" --exclude-dir="^/dev" --exclude-dir="^/proc" -l /var/log/clamav/clamav.log / > /dev/null 2>&1 &
