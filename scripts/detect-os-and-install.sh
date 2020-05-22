b37b7bd32fbf300f2dd93121640ee0b15da0bf21
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-05-22 18:10:22 +0200

    Update detect-os-and-install.sh

--- scripts/detect-os-and-install.sh
+++ scripts/detect-os-and-install.sh
@@ -2,23 +2,24 @@
 
 detect=$(cat /etc/*-release | grep 'ID=')
 if [[ $detect == *"debian"* ]]; then
-	sudo apt-get update
-	sudo apt-get install clamav
-	sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
+	apt-get update
+	apt-get install clamav
+	sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
 elif [[ $detect == *"ubuntu"* ]]; then
-	sudo apt-get update
-	sudo apt-get install clamav
-	sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
+	apt-get update
+	apt-get install clamav
+	sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
 elif [[ $detect == *"centos"* ]]; then
-    sudo yum install -y epel-release 
-    sudo yum install -y clamav 
-    sudo sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
+    yum install -y epel-release 
+    yum install -y clamav 
+    sed -i -e "s/^Example/#Example/" /etc/freshclam.conf
 elif [[ $detect == *"alpine"* ]]; then
-	sudo apk add clamav
-	sudo sed -i -e "s/^Example/#Example/" /etc/clamav/freshclam.conf
+	apk add clamav
+	sed -i -e "s/^Example/#Example/" /etc/clamav/freshclam.conf
 fi
 
-sudo freshclam
-sudo touch /var/log/clamav/clamav.log
-sudo chgrp clamupdate /var/log/clamav/clamav.log
-sudo chmod 1755 /var/log/clamav/clamav.log
+freshclam
+mkdir /var/log/clamav
+touch /var/log/clamav/clamav.log
+chgrp clamupdate /var/log/clamav/clamav.log
+chmod 1755 /var/log/clamav/clamav.log
