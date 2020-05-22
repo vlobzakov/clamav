876a535bfc2ae5dd25021012055d6f8b27e66d43
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-05-22 18:32:02 +0200

    Update scan.sh

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -3,5 +3,7 @@
 date=$(date)
 
 freshclam
-echo "This Scan has been started at" $date >> /var/log/clamav/clamav.log
+echo "-------------------------------------------------------------------------------" >> /var/log/clamav/clamav.log
+echo "" >> /var/log/clamav/clamav.log
+echo "This following Scan has been started at" $date >> /var/log/clamav/clamav.log
 clamscan --infected --recursive --remove --exclude-dir="^/sys" --exclude-dir="^/dev" --exclude-dir="^/proc" -l /var/log/clamav/clamav.log / > /dev/null 2>&1 &
