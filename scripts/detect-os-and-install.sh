f4d68cea686aa7fcccb575841d3802274e4c20ef
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-07-12 22:50:22 +0200

    add more logging

--- scripts/detect-os-and-install.sh
+++ scripts/detect-os-and-install.sh
@@ -20,8 +20,18 @@ elif [[ $detect == *"alpine"* ]]; then
 	sed -i -e "s/^Example/#Example/" /etc/clamav/freshclam.conf
 fi
 
-freshclam
+#update the config file to use correct log-file
+if [[ $detect == *"alpine"* ]]; then
+	sed -i -e 's@^#UpdateLogFile /var/log/freshclam.log@UpdateLogFile /var/log/clamav/clamav.log@' /etc/clamav/freshclam.conf
+	sed -i -e 's@^#LogFileMaxSize 2M@LogFileMaxSize 0@' /etc/clamav/freshclam.conf
+else
+	sed -i -e 's@^#UpdateLogFile /var/log/freshclam.log@UpdateLogFile /var/log/clamav/clamav.log@' /etc/freshclam.conf
+	sed -i -e 's@^#LogFileMaxSize 2M@LogFileMaxSize 0@' /etc/freshclam.conf
+fi
+
 mkdir /var/log/clamav
 touch /var/log/clamav/clamav.log
+chown clamav /var/log/clamav/clamav.log
 chgrp clamupdate /var/log/clamav/clamav.log
-chmod 1755 /var/log/clamav/clamav.log
+chmod 0764 /var/log/clamav/clamav.log
+freshclam
